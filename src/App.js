4a478b15450c30a751bbf1f72b2b54ff93581e5e
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-10 10:38:55 -0500

    added basic router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// style
 import './App.css';
+// import components
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Here is my React Portfolio!</h1>
-    </div>
+    <Router>
+      <>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="*" component={NoMatch} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
