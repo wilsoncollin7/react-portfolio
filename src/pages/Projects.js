ba641fa667fe7d1c293d7dd8eda5fb31e1c11740
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-12 10:27:15 -0500

    added website photos to the docs

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import components
 import Wrapper from "../components/Wrapper/Wrapper";
 import Header from "../components/Header/Header";
+import Container from "../components/Container/Container";
 import Footer from "../components/Footer/Footer";
 
 
@@ -9,8 +10,10 @@ function Projects() {
   return (
     <>
     <Wrapper>
-      <Header title="Projects"/>
-    </Wrapper>
+      <Container className="projectContainer">
+        <Header title="Projects"/>
+      </Container>
+    </Wrapper> 
     <Footer />
   </>
   )
