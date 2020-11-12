c009af26e61859d59348d9fb7bfd64176001a0ac
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-12 14:24:42 -0500

    production

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import components
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
