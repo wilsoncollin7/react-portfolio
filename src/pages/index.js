32d6953d39ab3d8d40968fb301f1d908859a5839
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-11-12 15:56:26 -0500

    added index.js to pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import NoMatch from "./NoMatch";
+import Projects from "./Projects";
+
+export {
+  Home,
+  About,
+  Contact,
+  NoMatch,
+  Projects
+}
